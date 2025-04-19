e07b13164e98f71e04137395feea6e5a0556dc7e
darwin403 <skdcodes@gmail.com>
2025-04-19 15:34:29 +0530

    add: upgrade next.js to 15.3.1. implemented sentry.io with sentry wizard

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://80b288b8f270949079427a9aefc2fc08@o4509179278131200.ingest.us.sentry.io/4509179286061056",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
